b530a2668450dccb98b2bf01585fb4386981686b
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-10-22 22:25:58 +0530

    Add reveal image animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { CSSRulePlugin, Power2 } from "gsap/all";
 import "./App.scss";
 
 import People from "./images/front.webp";
+gsap.registerPlugin(CSSRulePlugin);
 
 function App() {
+  let containerRef = useRef(null);
+  let imageRef = useRef(null);
+  let psedoRef = CSSRulePlugin.getRule(".image-container:after");
+
+  let tl = gsap.timeline();
+
+  useEffect(() => {
+    console.log(psedoRef);
+    tl.to(containerRef.current, {
+      duration: 0,
+      visibility: "visible",
+    })
+      .to(psedoRef, {
+        duration: 1.4,
+        width: "0%",
+        ease: Power2.easeInOut,
+      })
+      .from(imageRef.current, {
+        duration: 1.2,
+        scale: 1.4,
+        delay: -1.2,
+      });
+    //eslint-disable-next-line
+  }, [People]);
+
   return (
     <>
       <section className="Main">
-        <div className="Container">
+        <div className="Container" ref={containerRef}>
           <>
-            <div className="image-container">
-              <img src={People} alt="People" />
+            <div className="image-container" >
+              <img src={People} alt="People" ref={imageRef}/>
             </div>
           </>
         </div>
